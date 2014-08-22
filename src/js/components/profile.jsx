252911baa01cba8be7a7b3d55d0cffad4b10a909
Logan Howard <loganh@pobox.com>
2014-08-21 19:08:03 -0700

    tweak badge rendering for profile

--- src/js/components/profile.jsx
+++ src/js/components/profile.jsx
@@ -37,32 +37,32 @@ var Profile = React.createClass({
           <hr />
           <h2>BADGES</h2>
           <h4 className="subheader">Outreach:</h4>
-          <ul className="small-block-grid-4">
+          <ul className="small-block-grid-6">
             {this.renderBadgesByCategory(userBadges, 'Outreach', candidateBadges)}
           </ul>
           <br />
           <h4 className="subheader">Mechanical:</h4>
-          <ul className="small-block-grid-4">
+          <ul className="small-block-grid-6">
             {this.renderBadgesByCategory(userBadges, 'Mechanical', candidateBadges)}
           </ul>
           <br />
           <h4 className="subheader">Electrical:</h4>
-          <ul className="small-block-grid-4">
+          <ul className="small-block-grid-6">
             {this.renderBadgesByCategory(userBadges, 'Electrical', candidateBadges)}
           </ul>
           <br />
           <h4 className="subheader">Software:</h4>
-          <ul className="small-block-grid-4">
+          <ul className="small-block-grid-6">
             {this.renderBadgesByCategory(userBadges, 'Software', candidateBadges)}
           </ul>
           <br />
-          <h4 className="subheader">Public Relations:</h4>
-          <ul className="small-block-grid-4">
+          <h4 className="subheader">PR:</h4>
+          <ul className="small-block-grid-6">
             {this.renderBadgesByCategory(userBadges, 'PR', candidateBadges)}
           </ul>
           <br />
           <h4 className="subheader">Other:</h4>
-          <ul className="small-block-grid-4">
+          <ul className="small-block-grid-6">
             {this.renderBadgesByCategory(userBadges, 'Other', candidateBadges)}
           </ul>
         </div>
@@ -79,26 +79,19 @@ var Profile = React.createClass({
             <div className="large-8 columns">
               <h5 style={{color: 'orange'}}>Outreach</h5>
               <h5 style={{color: 'orange'}}>Mechanical</h5>
-              <h5 style={{color: 'orange'}}>  Gearbox Design</h5>
-              <h5 style={{color: 'orange'}}>  Motor Physics</h5>
               <h5 style={{color: 'purple'}}>Electrical</h5>
-              <h5 style={{color: 'purple'}}>  Printed Circuit Boards</h5>
               <h5 style={{color: 'green'}}>Software</h5>
-              <h5 style={{color: 'blue'}}>Public Relations</h5>
+              <h5 style={{color: 'blue'}}>PR</h5>
               <h5 style={{color: 'red'}}>Other</h5>
             </div>
             <div className="large-4 columns">
               <h5 style={{color: 'orange'}}>4</h5>
               <h5 style={{color: 'orange'}}>4++</h5>
-              <h5> </h5>
-              <h5> </h5>
               <h5 style={{color: 'purple'}}>2+</h5>
-              <h5> </h5>
               <h5 style={{color: 'green'}}>0</h5>
               <h5 style={{color: 'blue'}}>1</h5>
               <h5 style={{color: 'red'}}>3</h5>
             </div>
-            <hr />
           </div>
         </div>
       </div>
@@ -131,7 +124,7 @@ var Profile = React.createClass({
     return <li key={badge.id}>
       <a href={'/badge/' + badge.id}><img
         src={'/static/assets/badges/'
-          + badge.category + '/' + badge.name + '/medium.jpg'}
+          + badge.category + '/' + badge.name + '/small.jpg'}
         className={'badge ' + status} /></a>
     </li>;
   },
