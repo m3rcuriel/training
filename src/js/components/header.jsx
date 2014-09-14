18f593a1654be40512476fa5588fa769ea927ef3
Logan Howard <logan@oflogan.com>
2014-09-14 09:15:35 -0700

    add calendar

--- src/js/components/header.jsx
+++ src/js/components/header.jsx
@@ -37,6 +37,9 @@ var LoggedInBar = React.createClass({
           <li><a className="button" href="/badges">All Badges</a></li>
           <li><a className="button success" href="/">Home</a></li>
           <li><a className="button success" href="/settings">Settings</a></li>
+          {authorized
+            ? null
+            : <li><a className="button success" href="/important-info">Important info</a></li>}
         </ul>
       </div>
       <div className="button-bar right">
--- src/js/components/important-info.jsx
+++ src/js/components/important-info.jsx
@@ -14,6 +14,10 @@ var ImportantInfo = React.createClass({
     }
 
     var message = converter.makeHtml(this.state.message);
+    var calendar = '<iframe src="https://www.google.com/calendar/embed?height=600\
+&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=fhsroboticsmentors%40gmail.com&amp;\
+color=%23A32929&amp;ctz=America%2FLos_Angeles" style=" border-width:0 "\
+width="800" height="600" frameborder="0" scrolling="no"></iframe>';
 
     return <main className="about">
       <div className="row">
@@ -24,6 +28,12 @@ var ImportantInfo = React.createClass({
 
         <div className="columns large-10 large-centered">
           <span dangerouslySetInnerHTML={{__html: message}} />
+          <br />
+          <hr />
+          <br />
+          <span dangerouslySetInnerHTML={{__html: calendar}} />
+          <br />
+          <br />
           <br />
           <a className="button right" href="/">Go to your profile ➤</a>
           <br />
