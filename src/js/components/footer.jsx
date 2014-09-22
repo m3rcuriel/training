b37edc8ac191ced35dd794b9e829b49ce0c315d5
Logan Howard <logan@oflogan.com>
2014-09-22 15:06:57 -0700

    add sohini to footer

--- src/js/components/footer.jsx
+++ src/js/components/footer.jsx
@@ -6,16 +6,21 @@ var Footer = React.createClass({
       <div className="large-12 columns">
         <hr />
         <div className="row">
-          <div className="large-6 columns">
-            <p><a href="mailto:admin@oflogan.com">Contact Logan</a> for code access.</p>
+
+          <div className="large-7 columns">
+            <p><a href="mailto:admin@oflogan.com">Contact Logan</a> for code
+              / <a href="mailto:sohiniss@gmail.com">Sohini</a> for admin
+              issues or missing badges.</p>
           </div>
-          <div className="large-6 columns">
+
+          <div className="large-5 columns">
             <ul className="inline-list right">
               <li><a href="/about">About</a></li>
               <li><a href="/important-info">Important Info</a></li>
               <li><a href="/about/code">About the code</a></li>
             </ul>
           </div>
+
         </div>
       </div>
     </footer>;
