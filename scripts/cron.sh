52f6f9bd97bfdc8ddcc56755cbfa6c6bbf626605
Logan Howard <logan@oflogan.com>
2014-10-07 00:46:23 -0700

    change cron script

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-nodeProcess=`ps ax | fgrep node | awk '{print $1}' | sed -n 1p`
-if [ ! $nodeProcess ]; then
+nodeProcess=`ps ax | fgrep node | awk '{print $5}' | sed -n 1p`
+if [ $nodeProcess != "node" ]; then
   cd /root/training
   bash scripts/restart.sh
 fi
