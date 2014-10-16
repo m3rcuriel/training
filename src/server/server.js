f4c5d7783cbc3475c73cc8823c076b68b963e8a1
Logan Howard <logan@oflogan.com>
2014-10-16 14:48:08 -0700

    try to fix

--- src/server/server.js
+++ src/server/server.js
@@ -32,14 +32,14 @@ ex.use(function(req, res, next) {
 
         setImmediate(function reactRenderOnNextTick () {
             var markup = React.renderComponentToString(app);
-
+//ga("create", "UA-54088466-2", "auto");\
             var googleAnalytics = '<script>\
 (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){\
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\
 })(window,document,"script","//www.google-analytics.com/analytics.js","ga");\
-ga("create", "UA-54088466-2", "auto");\
-ga("send", "pageview");';
+ga("create", "UA-54088466-2", ' + userId ? '{"userId": "' + userId.toS() + '"});' : 'auto);' +
+'ga("send", "pageview");';
 
             var html = '<!DOCTYPE html>\
                 <html>\
