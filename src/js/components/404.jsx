ec830a8c9bad926c6991e968da0e66a05838c67e
Logan Howard <logan@oflogan.com>
2014-11-18 22:49:37 -0800

    make more professional

--- src/js/components/404.jsx
+++ src/js/components/404.jsx
@@ -5,7 +5,12 @@ var applicationState = require('../state/application.js');
 var E404 = React.createClass({
   render: function() {
     if (applicationState().auth.user.val()) {
-    var userFirstName = applicationState().auth.user.first_name.val();
+      var userFirstName = applicationState().auth.user.first_name.val();
+    }
+
+    if (userFirstName) {
+      var lead = (applicationState().auth.user.permissions.val() === 'lead');
+      var link = "http://www.goodfuckingdesignadvice.com/advice/201/";
     }
 
     return <main className="login">
@@ -13,8 +18,14 @@ var E404 = React.createClass({
         <br />
         <h1>404 – Page Not Found.</h1>
         <br /><br />
-        <h3>You done bad, {userFirstName || 'friend'}. Real bad.</h3>
-        <h5 className="subheader">(Click on the back button to check yourself. Before you wreck yourself.)</h5>
+        {lead
+          ? <h3>We're sorry, but the page you wanted was not fucking found.</h3>
+          : <h3>You done bad, {userFirstName || 'friend'}. Real bad.</h3>}
+        {lead
+          ? <h5 className="subheader">
+              (Maybe you need <a href={link}>some life advice.</href>)
+            </h5>
+          : <h5 className="subheader">(Click on the back button to check yourself. Before you wreck yourself.)</h5>}
       </div>
     </main>;
   },
